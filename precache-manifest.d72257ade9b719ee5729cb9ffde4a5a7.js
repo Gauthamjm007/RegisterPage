a5045d365f19408fd31d5b864b8ecf1668be6900
Gauthamjm007 <j.mgautham15@gmail.com>
2020-06-21 21:03:42 +0530

    Updates

---
+++ precache-manifest.d72257ade9b719ee5729cb9ffde4a5a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0363f8b4811c24ac45abbbf75064df50",
+    "url": "/RegisterPage/index.html"
+  },
+  {
+    "revision": "1533c31c59c1c4095e60",
+    "url": "/RegisterPage/static/css/3.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a27bcff9368507ac3e62",
+    "url": "/RegisterPage/static/css/main.8d8fe9b7.chunk.css"
+  },
+  {
+    "revision": "2b8f9203d637003186b8",
+    "url": "/RegisterPage/static/js/2.bf8a704e.chunk.js"
+  },
+  {
+    "revision": "71d8bc3ec214ae9da98355664aee2f9c",
+    "url": "/RegisterPage/static/js/2.bf8a704e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1533c31c59c1c4095e60",
+    "url": "/RegisterPage/static/js/3.79215521.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RegisterPage/static/js/3.79215521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78321d3cb194b686b7f",
+    "url": "/RegisterPage/static/js/4.6d2e3b00.chunk.js"
+  },
+  {
+    "revision": "a27bcff9368507ac3e62",
+    "url": "/RegisterPage/static/js/main.7a84504d.chunk.js"
+  },
+  {
+    "revision": "1dc872af5eb7733a67cf",
+    "url": "/RegisterPage/static/js/runtime-main.8b262157.js"
+  },
+  {
+    "revision": "e31c8218a5c92d98544b50a391c93a46",
+    "url": "/RegisterPage/static/media/upload_icon.e31c8218.png"
+  }
+]);
